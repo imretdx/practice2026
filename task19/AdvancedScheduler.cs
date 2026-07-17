6740ee223e89e3cdc9d62bc20c74473e466eaa2c
Belousov <imretdx@gmail.com>
2026-07-17 20:46:36 +0600

    chore: развернуты сигнатуры методов и конструкторов под классический C#

--- task19/AdvancedScheduler.cs
+++ task19/AdvancedScheduler.cs
@@ -1,32 +1,140 @@
-using System; using System.Collections.Concurrent; using System.Threading;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
 namespace task19;
-public interface ICommand { void Execute(); }
-public class TestCommand : ICommand {
-    private readonly int _id; public int Counter { get; private set; } = 0;
-    public TestCommand(int id) => _id = id;
-    public void Execute() { Counter++; Console.WriteLine($"Поток {_id} вызов {Counter}"); }
+
+public interface ICommand
+{
+    void Execute();
 }
-public class HardStopCommand : ICommand {
-    private readonly AdvancedSchedulerServerThread _srv; public HardStopCommand(AdvancedSchedulerServerThread srv) => _srv = srv;
-    public void Execute() => _srv.HardStop();
+
+public class TestCommand : ICommand
+{
+    private readonly int _id;
+    private int _counter = 0;
+
+    public int Counter
+    {
+        get 
+        { 
+            return _counter; 
+        }
+    }
+
+    public TestCommand(int id)
+    {
+        _id = id;
+    }
+
+    public void Execute()
+    {
+        _counter++;
+        Console.WriteLine($"Поток {_id} вызов {_counter}");
+    }
 }
-public class AdvancedSchedulerServerThread {
-    private readonly BlockingCollection<ICommand> _ext = new(); private readonly ConcurrentQueue<ICommand> _sch = new(); private readonly Thread _th; private bool _stop = false;
-    public Thread UnderlyingThread => _th;
-    public AdvancedSchedulerServerThread() => _th = new Thread(() => {
-        while (!_stop) {
-            while (_ext.TryTake(out var n)) _sch.Enqueue(n);
-            if (!_sch.IsEmpty) {
-                if (_sch.TryDequeue(out var c)) {
-                    try { c.Execute(); } catch {}
-                    if (_stop) break;
-                    if (c is TestCommand t && t.Counter < 3) _sch.Enqueue(t);
+
+public class HardStopCommand : ICommand
+{
+    private readonly AdvancedSchedulerServerThread _srv;
+
+    public HardStopCommand(AdvancedSchedulerServerThread srv)
+    {
+        _srv = srv;
+    }
+
+    public void Execute()
+    {
+        _srv.HardStop();
+    }
+}
+
+public class AdvancedSchedulerServerThread
+{
+    private readonly BlockingCollection<ICommand> _ext = new BlockingCollection<ICommand>();
+    private readonly ConcurrentQueue<ICommand> _sch = new ConcurrentQueue<ICommand>();
+    private readonly Thread _th;
+    private bool _stop = false;
+
+    public Thread UnderlyingThread
+    {
+        get 
+        { 
+            return _th; 
+        }
+    }
+
+    public AdvancedSchedulerServerThread()
+    {
+        _th = new Thread(new ThreadStart(RunSchedulerLoop));
+    }
+
+    public void Start()
+    {
+        _th.Start();
+    }
+
+    public void HardStop()
+    {
+        _stop = true;
+    }
+
+    public void AddCommand(ICommand c)
+    {
+        _ext.Add(c);
+    }
+
+    private void RunSchedulerLoop()
+    {
+        while (!_stop)
+        {
+            while (_ext.TryTake(out var nextCommand))
+            {
+                _sch.Enqueue(nextCommand);
+            }
+
+            if (!_sch.IsEmpty)
+            {
+                if (_sch.TryDequeue(out var activeTask))
+                {
+                    try
+                    {
+                        activeTask.Execute();
+                    }
+                    catch (Exception)
+                    {
+                        // Сбой операции
+                    }
+
+                    if (_stop)
+                    {
+                        break;
+                    }
+
+                    if (activeTask is TestCommand testTask)
+                    {
+                        if (testTask.Counter < 3)
+                        {
+                            _sch.Enqueue(testTask);
+                        }
+                    }
                 }
                 Thread.Sleep(1);
             }
-            else { try { if (_ext.TryTake(out var f, Timeout.Infinite)) _sch.Enqueue(f); } catch { break; } }
+            else
+            {
+                try
+                {
+                    if (_ext.TryTake(out var standaloneCommand, Timeout.Infinite))
+                    {
+                        _sch.Enqueue(standaloneCommand);
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
         }
-    });
-    public void Start() => _th.Start(); public void HardStop() => _stop = true;
-    public void AddCommand(ICommand c) => _ext.Add(c);
+    }
 }
