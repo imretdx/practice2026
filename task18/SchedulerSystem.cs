6d9dbb82f51675ab93b7d5ee5cecc1e86df432b7
Belousov <imretdx@gmail.com>
2026-07-17 20:46:10 +0600

    chore: оптимизация логики циклического квантования планировщика

--- task18/SchedulerSystem.cs
+++ task18/SchedulerSystem.cs
@@ -1,23 +1,125 @@
-using System; using System.Collections.Concurrent; using System.Threading;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
 namespace task18;
-public interface ICommand { void Execute(); bool IsCompleted { get; } }
-public interface IScheduler { bool HasCommand(); ICommand Select(); void Add(ICommand cmd); }
-public class RoundRobinScheduler : IScheduler {
-    private readonly ConcurrentQueue<ICommand> _q = new();
-    public bool HasCommand() => !_q.IsEmpty;
-    public void Add(ICommand cmd) => _q.Enqueue(cmd);
-    public ICommand Select() => _q.TryDequeue(out var cmd) ? cmd : throw new InvalidOperationException();
+
+public interface ICommand
+{
+    void Execute();
+    bool IsCompleted { get; }
 }
-public class SchedulerServerThread {
-    private readonly BlockingCollection<ICommand> _ext = new(); private readonly IScheduler _sch; private readonly Thread _th; private bool _stop = false;
-    public Thread UnderlyingThread => _th;
-    public SchedulerServerThread(IScheduler sch) { _sch = sch; _th = new Thread(() => {
-        while (!_stop) {
-            while (_ext.TryTake(out var n)) _sch.Add(n);
-            if (_sch.HasCommand()) { var c = _sch.Select(); try { c.Execute(); } catch {} if (!c.IsCompleted) _sch.Add(c); Thread.Sleep(1); }
-            else { try { if (_ext.TryTake(out var f, Timeout.Infinite)) _sch.Add(f); } catch { break; } }
+
+public interface IScheduler
+{
+    bool HasCommand();
+    ICommand Select();
+    void Add(ICommand cmd);
+}
+
+public class RoundRobinScheduler : IScheduler
+{
+    private readonly ConcurrentQueue<ICommand> _q = new ConcurrentQueue<ICommand>();
+
+    public bool HasCommand()
+    {
+        return !_q.IsEmpty;
+    }
+
+    public void Add(ICommand cmd)
+    {
+        _q.Enqueue(cmd);
+    }
+
+    public ICommand Select()
+    {
+        if (_q.TryDequeue(out var cmd))
+        {
+            return cmd;
         }
-    });}
-    public void Start() => _th.Start(); public void Stop() => _stop = true;
-    public void AddCommand(ICommand c) => _ext.Add(c);
+        throw new InvalidOperationException("В планировщике отсутствуют доступные команды.");
+    }
+}
+
+public class SchedulerServerThread
+{
+    private readonly BlockingCollection<ICommand> _ext = new BlockingCollection<ICommand>();
+    private readonly IScheduler _sch;
+    private readonly Thread _th;
+    private bool _stop = false;
+
+    public Thread UnderlyingThread
+    {
+        get 
+        { 
+            return _th; 
+        }
+    }
+
+    public SchedulerServerThread(IScheduler sch)
+    {
+        _sch = sch;
+        _th = new Thread(new ThreadStart(ExecuteLoopProcessing));
+    }
+
+    public void Start()
+    {
+        _th.Start();
+    }
+
+    public void Stop()
+    {
+        _stop = true;
+    }
+
+    public void AddCommand(ICommand c)
+    {
+        _ext.Add(c);
+    }
+
+    private void ExecuteLoopProcessing()
+    {
+        while (!_stop)
+        {
+            // Переносим поступившие извне команды в циклический планировщик
+            while (_ext.TryTake(out var newCommand))
+            {
+                _sch.Add(newCommand);
+            }
+
+            if (_sch.HasCommand())
+            {
+                var currentCommand = _sch.Select();
+                try
+                {
+                    currentCommand.Execute();
+                }
+                catch (Exception)
+                {
+                    // Обработка непредвиденных сбоев
+                }
+
+                if (!currentCommand.IsCompleted)
+                {
+                    _sch.Add(currentCommand);
+                }
+                
+                Thread.Sleep(1);
+            }
+            else
+            {
+                try
+                {
+                    if (_ext.TryTake(out var firstCommand, Timeout.Infinite))
+                    {
+                        _sch.Add(firstCommand);
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+    }
 }
