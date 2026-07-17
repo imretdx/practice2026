d29d0cc2b422a4d6af188b9ced82280d1c5d9bb0
Belousov <imretdx@gmail.com>
2026-07-17 20:43:34 +0600

    chore: оптимизация и рефакторинг логики проверки строки

--- task01/StringExtensions.cs
+++ task01/StringExtensions.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Linq;
+using System.Text;
 
 namespace task01;
 
@@ -7,9 +7,44 @@ public static class StringExtensions
 {
     public static bool IsPalindrome(this string input)
     {
-        if (input == null) return false;
-        var cleanChars = input.ToLower().Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c)).ToArray();
-        if (cleanChars.Length == 0) return false;
-        return cleanChars.SequenceEqual(cleanChars.Reverse());
+        if (input == null)
+        {
+            return false;
+        }
+
+        // Очищаем строку в классическом стиле через StringBuilder (понятнее для студента)
+        StringBuilder cleanBuilder = new StringBuilder();
+        string lowerInput = input.ToLower();
+
+        for (int i = 0; i < lowerInput.Length; i++)
+        {
+            char c = lowerInput[i];
+            if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+            {
+                cleanBuilder.Append(c);
+            }
+        }
+
+        string cleanStr = cleanBuilder.ToString();
+        if (cleanStr.Length == 0)
+        {
+            return false;
+        }
+
+        // Классическая проверка палиндрома с двух концов (два указателя)
+        int left = 0;
+        int right = cleanStr.Length - 1;
+
+        while (left < right)
+        {
+            if (cleanStr[left] != cleanStr[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
